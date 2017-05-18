96670df9366c809f65f6ffa2b9adffb41a718568
ckdtj <ckdtj@CS_NOTE_2>
2017-05-18 10:32:53 +0900

    temp

---
+++ administrator_program/src/com/kitri/admin/database/dto/ComDto.java
@@ -0,0 +1,47 @@
+package com.kitri.admin.database.dto;
+
+public class ComDto {
+	private int comNum;
+	private int userNum;
+	private int isUse;
+	private int isBooking;
+	
+
+	public int getComNum() {
+		return comNum;
+	}
+
+	public void setComNum(int comNum) {
+		this.comNum = comNum;
+	}
+
+	public int getUserNum() {
+		return userNum;
+	}
+
+	public void setUserNum(int userNum) {
+		this.userNum = userNum;
+	}
+
+	public int getIsUse() {
+		return isUse;
+	}
+
+	public void setIsUse(int isUse) {
+		this.isUse = isUse;
+	}
+
+	public int getIsBooking() {
+		return isBooking;
+	}
+
+	public void setIsBooking(int isBooking) {
+		this.isBooking = isBooking;
+	}
+
+	@Override
+	public String toString() {
+		return comNum + "," + userNum + "," + isUse + "," + isBooking;
+	}
+
+}
